bdcc90d0741490fd28f8f7d9b2ac5ad8da29cba8
Srikanth Gurram <sgurram@amplify.com>
2016-03-22 16:55:35 +0530

    Added Test code for Session training on TestNG

---
+++ README.md
@@ -0,0 +1,11 @@
+# Sample code for Selenium Training Sessions
+
+##Session 1: Junit
+
+##Session 2: TestNG
+###Topics:
+* Setting up TestNG Project in eclipse
+* Create a class (MathOps) and some methods
+* Create a separate test class and write test cases for the MathOps methods
+* Introduction about @Test annotation
+* How to execute Test cases in eclipse 
---
+++ src/com/test/testng/TestMathOps.java
@@ -0,0 +1,38 @@
+package com.test.testng;
+
+import org.testng.annotations.Test;
+
+public class TestMathOps {
+	
+	MathOps math = new MathOps();
+	
+	@Test
+	public void testAdditon(){
+		int result = math.Addition(10, 5);
+		
+		if(result == 15){
+			System.out.println("Addition Test Passed");			
+		}else{
+			System.out.println("Additon Test Failed");
+		}
+	}
+	
+	@Test
+	public void testSubstraction(){
+		int result = math.Substraction(10, 5);
+		
+		if(result==5){
+			System.out.println("Substaction Test Passed");
+		}else{
+			System.out.println("Substaction Test Failed");
+		}
+	}
+	
+	@Test
+	public void message(){
+		System.out.println("Welcomce");
+	}
+
+
+}
+
